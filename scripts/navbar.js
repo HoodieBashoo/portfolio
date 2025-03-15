9c9a0830adbac2920d7ca39217e6785c17bc63cb
Tyler <tylerbaldwin00@me.com>
2025-03-14 19:05:15 -0600

    Minor code cleanup

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,28 +1,3 @@
-/*document.getElementById('overview-button').addEventListener('click', () => {
-  document.getElementById('top-of-page').scrollIntoView({
-    behavior: 'smooth',
-    block: 'end'
-  });
-})
-
-document.getElementById('engines-button').addEventListener('click', () => {
-  document.getElementById('engines').scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
-})
-
-document.getElementById('resume-button').addEventListener('click', () => {
-  
-})
-
-document.getElementById('contact-button').addEventListener('click', () => {
-  document.getElementById('contact').scrollIntoView({
-    behavior: 'smooth',
-    block: 'start'
-  });
-})*/
-
 document.querySelector('.hamburger-button').addEventListener('click', () => {
   document.querySelector('.hamburger-menu').classList.toggle('active');
 })
