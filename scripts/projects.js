778c90cf71529ad2130244bb274e80fc09e9ce76
Tyler <tylerbaldwin00@me.com>
2026-01-22 01:53:42 -0700

    Removed Enemy AI from the Pesticide entry, since it hardly counts compared to my new experience

--- scripts/projects.js
+++ scripts/projects.js
@@ -136,10 +136,6 @@ export const projects = [
         type: 'concept'
       },
       {
-        text: 'Enemy AI',
-        type: 'concept'
-      },
-      {
         text: 'Input Buffering',
         type: 'concept'
       },
